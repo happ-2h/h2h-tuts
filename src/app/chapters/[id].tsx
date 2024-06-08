a1dc20060b33606b04704fdb51043273854f8b71
happ-2h <hikonetgames@gmail.com>
2024-06-07 18:58:12 -0700

    Table of Contents component and styling

--- src/app/chapters/[id].tsx
+++ src/app/chapters/[id].tsx
@@ -1,11 +1,23 @@
 import { Stack, useLocalSearchParams } from "expo-router";
 import { MaterialCommunityIcons } from '@expo/vector-icons'
+import tableOfContents from "@assets/data/tableOfContents/tableOfContents";
+import { FlatList } from "react-native";
+import ChapterItem from "@/components/ChapterItem";
 
 const TOC = () => {
   const params = useLocalSearchParams();
 
+  const toc = tableOfContents[params.id];
+
   return (
     <>
+      <FlatList 
+        data={toc}
+        renderItem={({ item }) => <ChapterItem details={item} book={params.id} />}
+        contentContainerStyle={{ gap: 10, padding: 5 }}
+        style={{ backgroundColor: "#FAFAFA", height: "100%" }}
+      />
+
       <Stack.Screen options={{
         headerRight: () => <MaterialCommunityIcons name={params.icon} size={32} />,
         headerTitleAlign: "center"
---
+++ src/components/ChapterItem.tsx
@@ -0,0 +1,45 @@
+import { Link } from "expo-router";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+const ChapterItem = ({ details, book }) => {
+  // let [fontsLoaded, fontError]
+
+  return (
+    <Link href='/' asChild>
+      <Pressable style={styles.container}>
+        <Text style={styles.title}>{details.title}</Text>
+        <Text style={styles.desc}>{details.desc}</Text>
+      </Pressable>
+    </Link>
+  );
+};
+
+export default ChapterItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#FCFCFC",
+    borderRadius: 10,
+    padding: 10,
+    elevation: 5,
+    aspectRatio: 1.9
+  },
+  title: {
+    flex: 1,
+    color: "#399EE6",
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: '600',
+    fontFamily: "Montserrat_600SemiBold",
+    borderBottomColor: "#5C61660F",
+    borderStyle: "dotted",
+    borderBottomWidth: 2
+  },
+  desc: {
+    flex: 2,
+    textAlign: "center",
+    color: "#5C6166",
+    fontFamily: "Montserrat_400Regular",
+    fontSize: 20
+  }
+});
