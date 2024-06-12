0cf735689b2bd4e0110f90299c79d3e5f793dda1
happ-2h <hikonetgames@gmail.com>
2024-06-12 16:05:13 -0700

    C++ book cover

--- assets/data/bookCovers.tsx
+++ assets/data/bookCovers.tsx
@@ -5,7 +5,7 @@ const bookCovers: BookCover[] = [
     id: "cpp",
     title: "C++ Tutorial",
     desc: "C++20 tutorial for beginners",
-    img: null,
+    img: require("assets/imgs/book_covers/cover_cpp.png"),
     icon: ["language-cpp", "#399EE6"],
     status: []
   }
